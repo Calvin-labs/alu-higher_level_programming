da998c0c247047b3b8a1da9b732d6c8f7b90b275
Calvin-labs <calvin@alu.edu>
2026-06-19 18:16:59 +0000

    Fix 8-uppercase.py: reduce to 2 print calls, handle non-string input without crashing

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     """Print a string in uppercase, followed by a new line."""
-    for c in str:
-        if 97 <= ord(c) <= 122:
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print("{:c}".format(ord(c)), end="")
+    for c in "{}".format(str):
+        n = ord(c) - 32 if 97 <= ord(c) <= 122 else ord(c)
+        print("{:c}".format(n), end="")
     print()
